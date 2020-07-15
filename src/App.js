413f2533d4f681461e1bb9cf2db4231ff8167201
captnstarburst <conorphinchee@gmail.com>
2020-07-15 10:23:13 -0400

    added firebase config and initial routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </Router>
   );
 }
 
---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,101 @@
+import app from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/firebase-firestore';
+import 'firebase/firebase-storage';
+import 'firebase/functions';
+
+const config = {
+  apiKey: "AIzaSyB22bMbvLa0Caly9wVrBPlMOKl_euS6Vp4",
+  authDomain: "line-eyez.firebaseapp.com",
+  databaseURL: "https://line-eyez.firebaseio.com",
+  projectId: "line-eyez",
+  storageBucket: "line-eyez.appspot.com",
+  messagingSenderId: "829574106248",
+  appId: "1:829574106248:web:6d0c896dfb3af1e620233d",
+  measurementId: "G-ZDBMNDGK0W"
+};
+
+export default class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    app.auth().useDeviceLanguage();
+    app.functions();
+    this.wholesystem = app;
+    this.db = app.database();
+    this.firestore = app.firestore();
+    this.storage = app.storage();
+    this.auth = app.auth();
+  }
+
+  // *** Auth API ***
+  onAuthUserListener = (next, fallback) =>
+    this.auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        next(authUser);
+      } else {
+        fallback();
+      }
+    }
+  );
+
+  // Firebase authentications
+  doCreateUserWithEmailAndPassword(email, password) {
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+  doSignInWithEmailAndPassword(email, password) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+  doSignOut() {
+    return this.auth.signOut();
+  }
+  doPasswordReset(email) {
+    return this.auth.sendPasswordResetEmail(email);
+  }
+
+  // Firebase references
+  getAuth() {
+    return this.auth;
+  }
+  getDB() {
+    return this.db;
+  }
+  getDBRef(path) {
+    return this.db.ref(path);
+  }
+  getFieldValue() {
+    return this.wholesystem.firestore.FieldValue;
+  }
+  getFS() {
+    return this.firestore;
+  }
+  getFSDocRef(path) {
+    return this.firestore.doc(path);
+  }
+  getFSCollectionRef(path) {
+    return this.firestore.collection(path);
+  }
+  getFunctions() {
+    return app.functions();
+  }
+  getRecaptcha(c, info) {
+    return new this.wholesystem.auth.RecaptchaVerifier(c,info);
+  }
+  getStorageRef(path) {
+    return this.storage.ref(path);
+  }
+  getStorageRefFromURL(url) {
+    return this.storage.refFromURL(url);
+  }
+  getSystem() {
+    return this.wholesystem;
+  }
+
+  // Firebase timestamps
+  createTimestamp(seconds, nanoseconds) {
+    return new this.wholesystem.firestore.Timestamp(seconds, nanoseconds);
+  }
+  getTimestamp(date) {
+    return this.wholesystem.firestore.Timestamp.fromDate(date);
+  }
+}
