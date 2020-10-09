6ae35aeb6622fe01989423e718eb58756f1808f9
captnstarburst <conorphinchee@gmail.com>
2020-10-09 14:35:10 -0400

    drawer icons

--- src/components/UI/AppBar/index.js
+++ src/components/UI/AppBar/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -7,7 +8,18 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
+import Drawer from '@material-ui/core/Drawer';
 import Menu from '@material-ui/core/Menu';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+import QueueIcon from '@material-ui/icons/Queue';
+import SettingsApplicationsIcon from '@material-ui/icons/SettingsApplications';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,12 +31,24 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
 }));
 
 export default function MenuAppBar() {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -34,11 +58,57 @@ export default function MenuAppBar() {
     setAnchorEl(null);
   };
 
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+          <ListItem button >
+            <ListItemIcon> <AccountCircle /> </ListItemIcon>
+            <ListItemText primary={"My Account"} />
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon> <AddAPhotoIcon /> </ListItemIcon>
+            <ListItemText primary={"Add Photo"} />
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon> <QueueIcon /> </ListItemIcon>
+            <ListItemText primary={"Line-Eyes"} />
+          </ListItem>
+          
+        
+      </List>
+      <Divider />
+      <ListItem button>
+            <ListItemIcon>
+              <SettingsApplicationsIcon/> 
+            </ListItemIcon>
+            <ListItemText primary={"Settings"} />
+          </ListItem>
+      
+      
+    </div>
+  );
+
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge="start" className={classes.menuButton} onClick={toggleDrawer("left", true)} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
@@ -76,6 +146,9 @@ export default function MenuAppBar() {
           
         </Toolbar>
       </AppBar>
+      <Drawer anchor={"left"} open={state["left"]} onClose={toggleDrawer("left", false)}>
+            {list("left")}
+          </Drawer>
     </div>
   );
 }
