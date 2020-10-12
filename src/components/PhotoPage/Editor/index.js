5a9e6452d2b8b68449721f6136f84560508b0f01
captnstarburst <conorphinchee@gmail.com>
2020-10-12 16:33:44 -0400

    added save button on image editor

---
+++ src/components/PhotoPage/Editor/index.js
@@ -0,0 +1,82 @@
+import React  from 'react';
+import 'tui-image-editor/dist/tui-image-editor.css'
+import ImageEditor from '@toast-ui/react-image-editor'
+import SaveIcon from '@material-ui/icons/Save';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const myTheme = {
+  "menu.backgroundColor": "white",
+  "common.backgroundColor": "#151515",
+  "downloadButton.backgroundColor": "white",
+  "downloadButton.borderColor": "white",
+  "downloadButton.color": "black",
+};
+
+const useStyles = makeStyles((theme) => ({
+  buttonStyle: {
+    backgroundColor: 'white',
+    border: '1px solid #fdba3b',
+    borderTopColor: 'rgb(253, 186, 59)',
+    borderRightColor: 'rgb(253, 186, 59)',
+    borderBottomColor: 'rgb(253, 186, 59)',
+    borderLeftColor: 'rgb(253, 186, 59)',
+    borderColor: 'white',
+    marginRight: '40px',
+    marginTop: '10px',
+  }
+}));
+
+
+function HomePage() {
+
+  const classes = useStyles();
+
+  const imageEditor = React.createRef();  
+  
+  const saveImageToDisk = () => {
+    const imageEditorInst = imageEditor.current.imageEditorInst;
+    const data = imageEditorInst.toDataURL();
+    console.log(data)
+  };  
+  
+  return (
+    <div style={{background: "#151515"}}>
+      <div style={{display:'flex', flexDirection:"row-reverse"}}>
+        <Button
+            variant="contained"
+            color={"green"}
+            size="large"
+            startIcon={<SaveIcon />}
+            onClick={saveImageToDisk}
+            className={classes.buttonStyle}
+          >
+          Save
+        </Button>
+      </div>
+        
+        
+      <ImageEditor
+        includeUI={{
+          theme: myTheme,
+          menu: ['filter'],
+          initMenu: 'filter',
+          uiSize: {
+            height: '700px'
+          },
+          menuBarPosition: 'top'
+        }}
+        cssMaxHeight={window.innerHeight}
+        cssMaxWidth={window.innerWidth}
+        selectionStyle={{
+          cornerSize: 20,
+          rotatingPointOffset: 70,
+        }}
+        usageStatistics={true}
+        ref={imageEditor}
+      />
+
+        
+    </div>
+  );
+}export default HomePage;
--- src/components/PhotoPage/index.js
+++ src/components/PhotoPage/index.js
@@ -1,40 +1,13 @@
 import React from 'react'
 import AppBar from '../UI/AppBar'
-import 'tui-image-editor/dist/tui-image-editor.css'
-import ImageEditor from '@toast-ui/react-image-editor'
+import Editor from './Editor'
 import { withAuthorization } from '../Session';
-
-const myTheme = {
-    // Theme object to extends default dark theme.
-  };
 
 const PhotoPage = props => {
     return(
         <>
             <AppBar />
-            <ImageEditor
-    includeUI={{
-      loadImage: {
-        path: 'img/sampleImage.jpg',
-        name: 'SampleImage'
-      },
-      theme: myTheme,
-      menu: ['shape', 'filter'],
-      initMenu: 'filter',
-      uiSize: {
-        width: '1000px',
-        height: '700px'
-      },
-      menuBarPosition: 'bottom'
-    }}
-    cssMaxHeight={500}
-    cssMaxWidth={700}
-    selectionStyle={{
-      cornerSize: 20,
-      rotatingPointOffset: 70
-    }}
-    usageStatistics={true}
-  />
+            <Editor />
         </>
         
     )
